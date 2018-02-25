053ca18136e7e66a4d42174a0621ef0026ab05a9
Otsgolyak <36776260+Otsgolyak@users.noreply.github.com>
2018-02-25 11:11:14 +0200

    veriable name changed

--- HW.py
+++
@@ -1,6 +0,0 @@
-import math
-a=int(input('Введите переменную a: '))
-b=int(input('Введите переменную b: '))
-c=int(input('Введите переменную c: '))
-d=(math.log1p(c)/-b)**4+math.fabs(a)
-print('Ответ:',(d))
