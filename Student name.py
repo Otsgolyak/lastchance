ff6c62905956cacc4a219e34773959807f0a27ee
Otsgolyak <36776260+Otsgolyak@users.noreply.github.com>
2018-02-25 22:43:02 +0200

    Changed code according to new information learnd in lesson 2

--- Student name.py
+++ Student name.py
@@ -1,4 +1,5 @@
-student='Anton Golyak'
-a=student.split()
-list.reverse(a)
-print(a[0],a[1])
+name_surname = 'Anton Golyak'
+surname_name=name_surname.split()
+list.reverse(surname_name)
+#print('Фамилия студента: ', surname_name[0], '\nИмя студента: ', surname_name[1])
+print("Фамилия студента: %s \nИмя студента: %s" % (surname_name[0], surname_name[1]))
