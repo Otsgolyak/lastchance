187966542527c7aa4c49a172a79afa9856642245
Otsgolyak <36776260+Otsgolyak@users.noreply.github.com>
2018-02-26 03:16:52 +0200

    New example and some hotfixes of previous)

--- Student name.py
+++ Student name.py
@@ -1,5 +1,5 @@
 name_surname = 'Anton Golyak'
-surname_name=name_surname.split()
+surname_name = name_surname.split()
 list.reverse(surname_name)
 #print('Фамилия студента: ', surname_name[0], '\nИмя студента: ', surname_name[1])
 print("Фамилия студента: %s \nИмя студента: %s" % (surname_name[0], surname_name[1]))
--- Time Machine.py
+++ Time Machine.py
@@ -1,3 +1,9 @@
-date = '05.17.2018'
-date_lst = date.split('.')
-print (date_lst[1]+'.'+date_lst[0]+'.'+date_lst[2])
+##### не обращайте внимание на комменты)
+#month = (input('Введите месяц: '))
+#day = (input('Введите день: '))
+#year = (input('Введите год: '))
+#american_date = (month, day, year)
+#print(american_date)
+american_date = '05.17.2018'
+needed_date = american_date.split('.')
+print ("Дата: ", needed_date[1]+'.'+needed_date[0]+'.'+needed_date[2])
