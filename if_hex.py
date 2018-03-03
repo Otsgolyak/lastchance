add791eb3d5d4156a8d513de60b4dbb8b3125b58
Otsgolyak <36776260+Otsgolyak@users.noreply.github.com>
2018-03-03 22:43:41 +0200

    extra_hw trying re with hex)

---
+++ if_hex.py
@@ -0,0 +1,8 @@
+import re
+outputfile='../file.txt'
+my_file=open(outputfile, mode='w', encoding = 'latin_1')
+my_text = '0x012345,0xa1b2c3,0xdeadbeef,0x0x0x0x,0xabcdefg,0123abcd'
+text_look_for = r"[0][x][0-9a-f]+\,"
+all_results = re.findall(text_look_for, my_text)
+print(all_results)
+my_file.write(str(all_results))
