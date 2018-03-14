c1ff740c8523ab87d9a56cfbd5ac88a92d006091
Otsgolyak <36776260+Otsgolyak@users.noreply.github.com>
2018-03-14 14:02:20 +0200

    I guess this should be correct

--- angles.py
+++ angles.py
@@ -1,9 +1,11 @@
 import math
-ver = int(input("Введите значение угла: "))
+#ver = int(input("Введите значение угла: "))
 def degrees_2_radians (angle):
    radians =  (angle * math.pi) / 180
    return radians
-print(degrees_2_radians(ver))
+print('Косинусом угла 40\xb0 является %f \nКосинусом угла 45\xb0 является %f \nКосинусом угла 60\xb0 является %f \n' % 
+(math.cos(degrees_2_radians(40)), math.cos(degrees_2_radians(45)), math.cos(degrees_2_radians(60))) )
+
 # angle = float(input('Введите значение угла: '))
 # radians = math.radians(angle)                                    #####(angle * math.pi) / 180
 # cos_of_radians = math.cos(radians)
