183fd7fe345b97ceaf02b6df29605589cad88ac2
Anton Golyak <otsgolyak@gmail.com>
2018-04-23 14:24:28 +0300

    I guess It's correct...

--- pass_generator_29.py
+++ pass_generator_29.py
@@ -1,29 +1,28 @@
 import string
 import random
 
-pass_len = 8
 
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-num = string.digits
-lower_q = 3
-upper_q = 2
-num_q = 3
-qoute = [1, 1, 1]
-passw = ''
-def password_generator(lower_q, upper_q, num_q):
+def password_generator():
+    quotas = [1, 1, 1] # to guarantee min values
+    what_to_increase = 0
+    while sum(quotas) < 7:
+        what_to_increase = random.randint(0, len(quotas)-1)
+        quotas[what_to_increase] += random.randint(1, 2)
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase
     num = string.digits
     passw = ''
-    for i in range(lower_q):
+    
+    for i in range(quotas[0]):
         passw += random.choice(lower)
-    for i in range(upper_q):
+    for i in range(quotas[1]):
         passw += random.choice(upper)
-    for i in range(num_q):
+    for i in range(quotas[2]):
         passw += random.choice(num)
-    passw = list(passw)
-    random.shuffle(passw)
-    password = ''.join(passw)
-    return password
-print (password_generator(3, 2, 3))
+    if len(passw) == 7:
+        passw += '_'
+        passw = list(passw)
+        random.shuffle(passw)
+        
+    return ''.join(passw)
+print (password_generator())
