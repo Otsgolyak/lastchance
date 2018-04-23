efc4f2662acdea974638a9321067084eece0f22e
Anton Golyak <otsgolyak@gmail.com>
2018-04-23 14:37:14 +0300

    changed)

--- shuffle_list_25.py
+++ shuffle_list_25.py
@@ -1,26 +1,34 @@
+# import random
+
+
+# lst = []
+# for num in range(100):
+#     if num % 2 != 0:
+#         lst.append(num)
+# print(lst)                  
+# def shuffle_list(lst):
+#     empty_list = []
+#     count = 0
+#     while len(empty_list) != len(lst):
+#         for item in lst:
+#             item = random.choice(lst)
+#             if item not in empty_list:
+#                 empty_list.append(item)
+#                 count += 1
+#     return empty_list
+
 import random
+def shuffle_list ():
+    lst = [i for i in range(1, 100) if i % 2 != 0]
+    print(lst)
+    print(id(lst))
 
+    sequence_number = len(lst)
+    while sequence_number > 1:
+        sequence_number = sequence_number - 1
+        rand_number = random.randint(0, sequence_number)
+        lst[rand_number], lst[sequence_number] = lst[sequence_number], lst[rand_number]
+    return lst, id(lst)
+print(shuffle_list())
 
-lst = []
-for num in range(100):
-    if num % 2 != 0:
-        lst.append(num)
-print(lst)                  
-def shuffle_list(lst):
-    empty_list = []
-    count = 0
-    while len(empty_list) != len(lst):
-        for item in lst:
-            item = random.choice(lst)
-            if item not in empty_list:
-                empty_list.append(item)
-                count += 1
-    return empty_list
-
-           
         
-    # for item in lst:
-    #     rand_index = random.randint(0, len(empty_list)) 
-    #     empty_list.insert((rand_index), item)
-    #     return empty_list
-print(shuffle_list(lst))
