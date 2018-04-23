c01e0970fe3f91e4a74ff539943d0d9bd7f56462
Anton Golyak <otsgolyak@gmail.com>
2018-04-23 14:11:32 +0300

    test passed)))

--- hw_chess_23.py
+++ hw_chess_23.py
@@ -1,11 +1,12 @@
-def chess_reward (seed, n):
-   
+def chess_reward ():
+    ceed = 0
+    n = 64
     sum_of_corns = 0
     for cell in range(n):
-        total_number_of_corns = 2**cell
+        total_number_of_corns = 2**(cell-1)
         sum_of_corns += total_number_of_corns
         if total_number_of_corns >= 1000000:
-             return cell + 1, sum_of_corns 
-print(chess_reward(0, 64))
+             return cell - 1, total_number_of_corns - 1 
+print(chess_reward())
 
 #print('На %d клетке \nбудет лежать %d зёрен ' % (cell+1, total_number_of_corns))
