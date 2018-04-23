d82611adbdd3f8b81d7e5dbe17ad6892a26adc71
Anton Golyak <otsgolyak@gmail.com>
2018-04-23 11:40:14 +0300

    I don't understand why it does not pass the bot(((

--- hw_chess_23.py
+++ hw_chess_23.py
@@ -1,6 +1,11 @@
 def chess_reward (seed, n):
+   
+    sum_of_corns = 0
     for cell in range(n):
         total_number_of_corns = 2**cell
+        sum_of_corns += total_number_of_corns
         if total_number_of_corns >= 1000000:
-            return print('На %d клетке \nбудет лежать %d зёрен ' % (cell+1, total_number_of_corns))
-chess_reward(0, 64)
+             return cell + 1, sum_of_corns 
+print(chess_reward(0, 64))
+
+#print('На %d клетке \nбудет лежать %d зёрен ' % (cell+1, total_number_of_corns))
